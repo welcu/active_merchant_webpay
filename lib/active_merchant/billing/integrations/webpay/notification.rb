6cafa56f2830ee6ffb1e41988b9995c8966ee2ef
Sebastián Gamboa <me@sagmor.com>
2011-02-09 12:08:56 -0300

    Fix file encoding error in ruby 1.9

--- lib/active_merchant/billing/integrations/webpay/notification.rb
+++ lib/active_merchant/billing/integrations/webpay/notification.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'active_merchant/billing/integrations/notification'
 
 module ActiveMerchant #:nodoc:
--- lib/active_merchant/billing/integrations/webpay/version.rb
+++ lib/active_merchant/billing/integrations/webpay/version.rb
@@ -2,7 +2,7 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Webpay
-        VERSION = "0.0.3"
+        VERSION = "0.0.4"
       end
     end
   end
