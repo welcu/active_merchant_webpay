2f1aca396a755d8229ab7b19081cae7d1dcc79b0
Sebastián Gamboa <me@sagmor.com>
2011-02-09 11:49:35 -0300

    Bump version to 0.0.3

--- lib/active_merchant/billing/integrations/webpay/version.rb
+++ lib/active_merchant/billing/integrations/webpay/version.rb
@@ -2,7 +2,7 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Webpay
-        VERSION = "0.0.2"
+        VERSION = "0.0.3"
       end
     end
   end
